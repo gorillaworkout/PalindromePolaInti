553a69af3f5e9371ac50324f63ba115ede61a14c
Bayu Darmawan <darmawanbayu1@gmail.com>
2021-03-03 10:15:04 +0700

    update

--- Palindrome.js
+++ Palindrome.js
@@ -3,11 +3,12 @@
 const Palindrome = (kata) => {
   const karakter = kata.toLowerCase().replace(/[^a-z]/g, '') .split('');
   if (karakter.join('') === karakter.reverse().join('')) {
-    return true; 
-    } else { return false;
+    return `${kata} adalah Palindrome`; 
+    } else { return `${kata} bukanlah Palindrome`;
       }
     }
   const hasil = Palindrome("Kodok"); 
 
   console.log(hasil);
+  console.log(Palindrome('Malam'))
   
